6f7e2da21d493cfd64507a8b3c567c796bfdb77e
Taye Akinola <akinolataye831@gmail.com>
2020-06-19 17:31:58 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,81 @@
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  // Hide Results
+  document.getElementById('results').style.display = 'none';
+
+  // Show Loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
+
+// Calculate Results
+function calculateResults() {
+  console.log('Calculating...');
+
+  // UI Variables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute Monthly Payments
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+  } else {
+    showError('Please check your numbers');
+  }
+}
+
+// Show Error function
+function showError(error) {
+  // Create div
+  const errorDiv = document.createElement('div');
+
+  // Get parent Elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add Classes
+  errorDiv.className = 'alert alert-danger';
+
+  // Create text node and append
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 secs
+  setTimeout(function clearError() {
+    document.querySelector('.alert').remove();
+  }, 3000);
+
+  // Clear error
+  // function clearError() {
+  //   document.querySelector('.alert').remove();
+  // }
+
+  // Hide Loader
+  document.getElementById('loading').style.display = 'none';
+
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+}
